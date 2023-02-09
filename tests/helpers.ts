db9ac59611f922874c12ebf6678b2d4c947ee506
Yan Matheus <95008410+REFSZIN@users.noreply.github.com>
2023-02-09 20:41:05 -0300

    Update helpers.ts

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,8 +1,8 @@
 import { prisma } from "@/config";
 
 export async function cleanDb() {
-  await prisma.console.deleteMany({});
   await prisma.game.deleteMany({});
+  await prisma.console.deleteMany({});
 }
 
 
